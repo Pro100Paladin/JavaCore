2e039d67d09ad49d1580663dffbf2b213a17be40
Pro100Paladin <pro100paladin@icloud.com>
2024-07-02 12:52:07 +0300

    first commit

---
+++ src/lecture2/CaesarCode.java
@@ -0,0 +1,46 @@
+package lecture1;
+
+import java.util.Scanner;
+
+public class CaesarCode {
+    public static void caesarCodeEncoder() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст: ");
+        String code = scanner.nextLine();
+        int key = scanner.nextInt();
+
+        char[] symbol = new char[code.length()];
+
+        for (int i = 0; i < code.length(); i++) {
+            if (Character.isLetter(code.charAt(i)) && Character.isUpperCase(code.charAt(i)))
+                symbol[i] = (char) (code.charAt(i) + key);
+            else if (Character.isLetter(code.charAt(i)) && Character.isLowerCase(code.charAt(i)))
+                symbol[i] = (char) (code.charAt(i) + key);
+            else symbol[i] = code.charAt(i);
+        }
+        System.out.println(symbol);
+    }
+
+    public static void caesarCodeDecoder() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст: ");
+        String code = scanner.nextLine();
+        int key = scanner.nextInt() * -1;
+
+        char[] symbol = new char[code.length()];
+
+        for (int i = 0; i < code.length(); i++) {
+            if
+            (Character.isLetter(code.charAt(i)) && Character.isUpperCase(code.charAt(i)))
+                symbol[i] = (char) (code.charAt(i) + key);
+            else if
+            (Character.isLetter(code.charAt(i)) && Character.isLowerCase(code.charAt(i)))
+                symbol[i] = (char) (code.charAt(i) + key);
+            else
+                symbol[i] = code.charAt(i);
+        }
+        System.out.println(symbol);
+    }
+
+}
---
+++ src/lecture2/CyclicShiftingArray.java
@@ -0,0 +1,24 @@
+package lecture1;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class CyclicShiftingArray {
+
+    public static void shiftingArray() {
+        Scanner scanArray = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        String[] array = new String[Integer.parseInt(scanArray.nextLine())];
+        String arr = null;
+        System.out.println("Начните ввод");
+        for (int i = 0; i < array.length; i++) {
+            arr = scanArray.nextLine();
+            array[i] = arr;
+        }
+        System.out.println(Arrays.toString(array));
+//        for (int i = 0; )
+    }
+}
+
